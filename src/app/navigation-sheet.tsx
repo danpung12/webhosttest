b10b74407881e6e51b619ca3f96323f67ec81280
201910996 이은총 <aass6863@naver.com>
2025-07-10 21:24:42 +0900

    1234

--- src/app/navigation-sheet.tsx
+++ src/app/navigation-sheet.tsx
@@ -3,7 +3,6 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 import Link from "next/link";
 import { foods, travelMenuItems } from "./components/navbar/config";
-import { Logo } from "./logo";
 
 export const NavigationSheet = () => {
   return (
@@ -14,18 +13,17 @@ export const NavigationSheet = () => {
         </Button>
       </SheetTrigger>
       <SheetContent>
-        <Logo />
 
-        <div className="mt-12 text-base space-y-4">
+        <div className="mt-12 space-y-4 text-base">
           <Link href="#">Home</Link>
 
           <div>
             <div className="font-bold">Food</div>
-            <ul className="mt-2 space-y-3 ml-1 pl-4 border-l">
+            <ul className="pl-4 mt-2 ml-1 space-y-3 border-l">
               {foods.map((foodItem) => (
                 <li key={foodItem.title}>
                   <Link href="#" className="flex items-center gap-2">
-                    <foodItem.icon className="h-5 w-5 mr-2 text-muted-foreground" />
+                    <foodItem.icon className="w-5 h-5 mr-2 text-muted-foreground" />
                     {foodItem.title}
                   </Link>
                 </li>
@@ -35,11 +33,11 @@ export const NavigationSheet = () => {
 
           <div>
             <div className="font-bold">Travel</div>
-            <ul className="mt-2 space-y-3 ml-1 pl-4 border-l">
+            <ul className="pl-4 mt-2 ml-1 space-y-3 border-l">
               {travelMenuItems.map((item) => (
                 <li key={item.title}>
                   <Link href="#" className="flex items-center gap-2">
-                    <item.icon className="h-5 w-5 mr-2 text-muted-foreground" />
+                    <item.icon className="w-5 h-5 mr-2 text-muted-foreground" />
                     {item.title}
                   </Link>
                 </li>
